ef51f399e5450be6e3ab9dccd2af2ad8d0ff2a95
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-01-03 07:34:58 +0000

    Implement environment wrappers

--- wrappers/envs.py
+++ wrappers/envs.py
@@ -1,6 +1,9 @@
 """
 envs.py
 """
+from .atari_wrappers import make_atari, wrap_deepmind
+from .torch_wrappers import wrap_pytorch
+
 
 def make_env(env_id):
     """
@@ -18,14 +21,16 @@ def make_env(env_id):
         Wrapped OpenAI Gym environment.
     """
     if env_id in ['Pong']:
-        # TODO Create environment
+        # Create environment
         env_id = env_id + 'NoFrameskip-v4'
         env = make_atari(env_id)
 
-        # TODO Wrap environment to fit DeepMind-style environment
+        # Wrap environment to fit DeepMind-style environment
         env = wrap_deepmind(env, frame_stack=True)
 
-        # TODO Wrap environment for PyTorch agents
-        env = TorchWrapper(env)
+        # Wrap environment for PyTorch agents
+        env = wrap_pytorch(env)
     else:
         raise ValueError('{} is not a supported environment.'.format(env_id))
+
+    return env
