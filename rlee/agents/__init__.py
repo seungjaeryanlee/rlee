ff14563db7b1176165a3d3ea4544cb04f5e716eb
Seungjae Ryan Lee <seungjaeryanlee@gmail.com>
2019-03-04 09:48:17 +0900

    Unify NaiveDQN, DQN2013, and DQN2015 agents (#41)
    * Add NaiveDQN and DQN2013 configs using DQN2015
    
    * Remove Naive and DQN2013 agents from train_atari
    
    * Remove NaiveDQN and DQN2013 agents

--- rlee/agents/__init__.py
+++ rlee/agents/__init__.py
@@ -1,8 +1,6 @@
 """Reinforcement Learning agents."""
 
-from .NaiveDQNAgent import NaiveDQNAgent
-from .DQN2013Agent import DQN2013Agent
 from .DQN2015Agent import DQN2015Agent
 
 
-__all__ = ["NaiveDQNAgent", "DQN2013Agent", "DQN2015Agent"]
+__all__ = ["DQN2015Agent"]
